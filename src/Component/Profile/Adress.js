a68fc738b95a83a433013d136a5e60a5c6b8fc80
unknown <houissahedi@gmail.com>
2021-06-30 12:57:35 +0100

    add image and css

---
+++ src/Component/Profile/Adress.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Adress = () => {
+    return (
+        <div>
+           <h3> Rue sidi bakha beni khiar nabeul               </h3> 
+               
+        </div>
+    )
+}
+
+export default Adress
---
+++ src/Component/Profile/ProfilePhoto.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import me1 from '../../assets/me1.png'
+
+
+const ProfilePhoto = () => {
+    return (
+        <div>
+            <img src={me1} alt="not existe"  width="400px" />
+        </div>
+    )
+}
+
+export default ProfilePhoto
